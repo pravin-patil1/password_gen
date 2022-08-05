5988f6d05747f7f0568d627203b36627147a819f
Pravin Patil <pravincpcp@gmail.com>
2022-08-05 20:51:39 +0530

    Initial_commit

---
+++ password_gen.py
@@ -0,0 +1,36 @@
+""" 
+    This is a basic password generator program based on user requirement. 
+    * Accept password lenght from user
+    * Accept number of characters, Number of digits equired in random password from user
+    * Accept choice of special character from user based on @ ~ _ symbols
+"""
+import random,string
+
+sp_l = ['@' '~' '_']
+print('====================================')
+print('   Welcome to password generator')
+print('====================================\n\n')
+
+lengh = int(input('Enter Password Lenght : '))
+alpha =int(input('Enter No of Characters needs to be included : '))
+no =int(input('Enter No of Digits needs to be included : '))
+spl = int (input('Enter No of special characters required in password : '))
+l_pwd = []
+pwd = ''
+sm = alpha+no+spl
+ver = string.ascii_letters
+nos = string.digits
+if lengh == sm:
+    for i in range(alpha):
+        l_pwd.append(random.choice(ver))
+    for y in range(no):
+        l_pwd.append(random.choice(nos))
+    for z in range(spl):
+        l_pwd.append(random.choice(sp_l))
+    random.shuffle(l_pwd)
+    for num in l_pwd:
+        pwd=pwd+num
+    print('Random Password : ',pwd)
+else:
+    print('\U0001F61E Please enter correct password lenght \U0001F61E\nPassword Lenght and no of characters, digits and special chaacters not matching!!')
+
