d45d4107570dca4e0dce2c5d624a8745cb510dae
Pravin Patil <pravincpcp@gmail.com>
2022-08-05 21:31:15 +0530

    removed some code

--- password_gen.py
+++ password_gen.py
@@ -4,9 +4,9 @@
     * Accept number of characters, Number of digits equired in random password from user
     * Accept choice of special character from user based on @ ~ _ symbols
 """
-import random,string
+from pwd_gen import pwd_gen
 
-sp_l = ['@', '~', '_']
+
 print('====================================')
 print('   Welcome to password generator')
 print('====================================\n')
@@ -15,20 +15,6 @@ lengh = int(input('Enter Password Lenght : '))
 alpha =int(input('Enter No of Characters needs to be included : '))
 no =int(input('Enter No of Digits needs to be included : '))
 spl = int (input('Enter No of special characters required in password : '))
-l_pwd = []
-pwd = ''
-sm = alpha+no+spl
-if lengh == sm:
-    for i in range(alpha):
-        l_pwd.append(random.choice(string.ascii_letters))
-    for y in range(no):
-        l_pwd.append(random.choice(string.digits))
-    for z in range(spl):
-        l_pwd.append(str(random.choice(sp_l)))
-    random.shuffle(l_pwd)
-    for num in l_pwd:
-        pwd=pwd+num
-    print('Random Password : ',pwd)
-else:
-    print('\U0001F61E Please enter correct password lenght \U0001F61E\nPassword Lenght and no of characters, digits and special chaacters not matching!!')
+n_r = int (input('Enter Number of random passwods requireds : '))
+pwd_gen(lengh,alpha,no,spl,n_r)
 
---
+++ pwd_gen.py
@@ -0,0 +1,30 @@
+import random,string
+sp_l = ['@', '~', '_']
+
+def pwd_gen(l,c,d,s,rn):
+    l_pwd = []
+    pwd = ''
+    lt = c+d+s
+    rn_pwd =[]
+    while rn > 0:
+        if lt == l:
+            for i in range(c):
+                l_pwd.append(random.choice(string.ascii_letters))
+            for y in range(d):
+                l_pwd.append(random.choice(string.digits))
+            for z in range(s):
+                l_pwd.append(str(random.choice(sp_l)))
+            random.shuffle(l_pwd)
+            for num in l_pwd:
+                pwd=pwd+num
+            rn_pwd.append(pwd)
+            pwd=''
+            l_pwd=[]
+        else:
+            print('\U0001F61E Please enter correct password lenght \U0001F61E\nPassword Lenght and no of characters, digits and special chaacters not matching!!')
+        rn -= 1
+    print('Random Passwords as below (multiple passwords seperated by / ): ')
+    for i in rn_pwd:
+        print(i, end = ' / ')
+
+    
